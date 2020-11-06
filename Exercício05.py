36a1053101fd0377c22024983e68915f36f027df
ViniciusDamiani <vinidamiani03@hotmail.com>
2020-11-05 22:22:29 -0300

    feat: exercicio05

---
+++ Exercício05.py
@@ -0,0 +1,14 @@
+#funcao = lambda y: return y
+#h = funcao(4)
+#print(h)
+#Dado ou trecho de código acima, qual será o resultado de sua execução?
+#()4
+#()0
+#(X)erro de sintaxe
+#()retorno 4
+
+funcao = lambda y: return y
+h = funcao(4)
+print(h)
+#Resposta 5
+print("Erro de sintaxe")
